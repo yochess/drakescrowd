258b78b1827cd2aa8b18ba43e7d242ea2dd7e5d5
Daham Drake Wang <drakewang@gmail.com>
2016-08-28 19:04:33 -0700

    Come up with final logic of database.

--- api/utils/dbconfig.js
+++ api/utils/dbconfig.js
@@ -15,6 +15,32 @@ const Company = sequelize.define('company', {
   password: Sequelize.STRING
 });
 
+const Property = sequelize.define('property', {
+  name: Sequelize.STRING,
+  img: Sequelize.STRING,
+  type: Sequelize.STRING,
+  min: Sequelize.FLOAT,
+  irr: Sequelize.FLOAT,
+  cash: Sequelize.FLOAT,
+  marketPrice: Sequelize.FLOAT,
+  available: {type: Sequelize.BOOLEAN, defaultValue: true},
+  additionaldetails: Sequelize.STRING
+});
+
+const Investment = sequelize.define('investment', {
+  amount: Sequelize.FLOAT,
+  pending: {type: Sequelize.BOOLEAN, defaultValue: true},
+  approved: {type: Sequelize.BOOLEAN, defaultValue: false}
+});
+
+Property.belongsTo(Company);
+Company.hasMany(Property);
+
+Investment.belongsTo(Property);
+Investment.belongsTo(Investor);
+Property.hasMany(Investment);
+Investor.hasMany(Investment);
+
 sequelize.sync();
 
 export {
