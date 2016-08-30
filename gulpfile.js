ea006e886f1a756ba80a551a700df502a955ab45
Daham Drake Wang <drakewang@gmail.com>
2016-08-29 21:09:27 -0700

    Success on gulp!

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,17 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
 
-gulp.task('default', () => {
-    return gulp.src('src/**/*.js')
-        .pipe(babel({
-            presets: ['es2015']
-        }))
-        .pipe(gulp.dest('dist'));
+gulp.task('js', () => {
+  return gulp.src('src/**/*.js')
+    .pipe(babel({
+        presets: ['es2015']
+    }))
+      .pipe(gulp.dest('dist'));
 });
+
+gulp.task('rest', () => {
+  return gulp.src(['src/**/*', '!src/**/*.js'])
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', ['js', 'rest']);
